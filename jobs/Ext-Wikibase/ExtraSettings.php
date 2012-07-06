5d53380d2d4f16918bddeb0aa9de1473fded1285
Antoine Musso <hashar@free.fr>
2012-07-06 16:22:50 +0000

    (bug 37050) enable Jenkins for Wikidata
    This is still a bit experimental and currently silenced to prevent false
    alarms to be sent back to Gerrit.
    
    The ant script import mediawiki/core using latest revision of the
    Wikidata branch then install the extension and apply the latest change.
    
    Change-Id: I02b4b8ba1d4c16fc60eafc5fd2486415e9002cee

---
+++ jobs/Ext-Wikibase/ExtraSettings.php
@@ -0,0 +1,4 @@
+<?php
+require_once( "$IP/extensions/Wikibase/lib/WikibaseLib.php");
+require_once( "$IP/extensions/Wikibase/repo/Wikibase.php");
+require_once( "$IP/extensions/Wikibase/client/WikibaseClient.php");
