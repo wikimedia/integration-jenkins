d519953bba5155749c9c995bcdf2c508863f8472
Antoine Musso <hashar@free.fr>
2012-07-31 08:50:48 +0000

    Ext-Wikibase: load repo settings + Diff + StickToThatLanguage
    The wikibase build check out several extensions, some of them were not
    properly initialised or just not included.
    
    Change-Id: I50ffbd1e0ff9cbe8e05727d3b0a4926774df6520

--- jobs/Ext-Wikibase/ExtraSettings.php
+++ jobs/Ext-Wikibase/ExtraSettings.php
@@ -1,4 +1,7 @@
 <?php
 require_once( "$IP/extensions/Wikibase/lib/WikibaseLib.php");
 require_once( "$IP/extensions/Wikibase/repo/Wikibase.php");
+require_once( "$IP/extensions/Wikibase/repo/ExampleSettings.php");
 require_once( "$IP/extensions/Wikibase/client/WikibaseClient.php");
+require_once( "$IP/extensions/Diff/Diff.php");
+require_once( "$IP/extensions/Wikibase/StickToThatLanguage/StickToThatLanguage.php");
