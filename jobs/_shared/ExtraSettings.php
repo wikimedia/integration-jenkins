e2b87dbeef07addcee565277d2420a39c766c7b3
Antoine Musso <hashar@free.fr>
2012-08-28 09:08:45 +0000

    (bug 38882) use $wgDevelopmentWarnings
    Makes MediaWiki throw notices for some deprecated functions. This apply
    to any MediaWiki job.
    
    Change-Id: I3e534a260ceae4addfde1ca7a1d57c93262d1317

--- jobs/_shared/ExtraSettings.php
+++ jobs/_shared/ExtraSettings.php
@@ -2,3 +2,5 @@
 
 $wgEnableJavaScriptTest = true;
 $wgJavaScriptTestConfig['qunit']['testswarm-injectjs'] = '//integration.mediawiki.org/testswarm/js/inject.js' ;
+
+$wgDevelopmentWarnings = true; # bug 38882
