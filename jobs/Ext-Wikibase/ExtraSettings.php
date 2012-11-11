d00ea440ffaa0f06efa2ba9a339e22c0930c85d3
aude <aude.wiki@gmail.com>
2012-11-11 01:11:46 +0000

    remove wikibase client from jenkins wikibase repo settings
    - wikibase client can be setup as a separate job, but
    should not co-exist in the same wiki as the repo.
    
    Change-Id: I89047bd68d9d95f5ec1811d45b8ee040cc782bf3

--- jobs/Ext-Wikibase/ExtraSettings.php
+++ jobs/Ext-Wikibase/ExtraSettings.php
@@ -4,7 +4,6 @@ require_once( "$IP/extensions/UniversalLanguageSelector/UniversalLanguageSelecto
 require_once( "$IP/extensions/Wikibase/lib/WikibaseLib.php");
 require_once( "$IP/extensions/Wikibase/repo/Wikibase.php");
 #require_once( "$IP/extensions/Wikibase/repo/ExampleSettings.php");
-require_once( "$IP/extensions/Wikibase/client/WikibaseClient.php");
 require_once( "$IP/extensions/Diff/Diff.php");
 #require_once( "$IP/extensions/Wikibase/StickToThatLanguage/StickToThatLanguage.php");
 
