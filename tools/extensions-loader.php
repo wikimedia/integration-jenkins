0d6e693ecf7d823c2dd392c8cdaf0a3d0a8af68f
Antoine Musso <hashar@free.fr>
2013-01-14 15:39:41 +0100

    current extension should be loaded last
    Change-Id: I6655ee7540ecdaec83a0f5eeb43f775adb04d175

--- tools/extensions-loader.php
+++ tools/extensions-loader.php
@@ -6,6 +6,21 @@ $func_get_exts = function () {
 		echo "Aborting\n";
 		exit(1);
 	}
+
+	# Find out the current project:
+	$ZUUL_PROJECT = getenv( 'ZUUL_PROJECT' );
+
+	# Attempt to extract the extension name:
+	$currentExt = null;
+	$m = array();
+	if( preg_match_all(
+		'%^mediawiki/extensions/(.*)$%',
+		$ZUUL_PROJECT,
+		$m )
+	) {
+		$currentExt = $m[1][0];
+	}
+
 	$ext_to_load = array();
 	$ext_missing = array();
 	foreach( scandir("{$IP}/extensions/") as $extname ) {
@@ -29,6 +44,14 @@ $func_get_exts = function () {
 		echo "\n\nAborting\n";
 		exit(1);
 	}
+
+	# Make sure the current extension is loaded last
+	if( $currentExt ) {
+		$currentExtFile = $ext_to_load[$currentExt];
+		unset( $ext_to_load[$currentExt] );
+		$ext_to_load[$currentExt] = $currentExtFile;
+	}
+
 	return $ext_to_load;
 };
 
