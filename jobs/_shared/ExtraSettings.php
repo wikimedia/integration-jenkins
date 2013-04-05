4865711a94ced8751725c44e4d521c3bae709988
Timo Tijhof <ttijhof@wikimedia.org>
2013-04-05 02:51:45 +0200

    ExtraSettings: Remove settings for testswarm/qunit.
    The TestSwarm ones are obsolete, and the QUnit settings are
    already enforced from the qunit build.
    
    Change-Id: If26a9c93c9f530058c1cde784c81cffd4fbfbcae

--- jobs/_shared/ExtraSettings.php
+++ jobs/_shared/ExtraSettings.php
@@ -1,9 +1,5 @@
 <?php
 
-$wgEnableJavaScriptTest = true;
-$wgJavaScriptTestConfig['qunit']['testswarm-injectjs'] = '//integration.mediawiki.org/testswarm/js/inject.js' ;
-
 $wgDevelopmentWarnings = true; # bug 38882
 
-# Full stack traces
 $wgShowExceptionDetails = true; # bug 43059
