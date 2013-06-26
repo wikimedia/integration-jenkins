fc96c5598b44c27c4994ad80efb3b2e7deee074b
Antoine Musso <hashar@free.fr>
2013-06-26 17:04:53 +0200

    ExtraSettings.php sets tmpfs to Jenkins user homedir
    The hardcoded /var/lib/jenkins does not work whenever the job is being
    triggered on a slave which uses 'jenkins-slave' as a username and has
    the tmpfs mounted at /var/lib/jenkins-slave/tmpfs .
    
    ant script need to be updated as well.
    
    Change-Id: I0f48feb4cf7106ac53a8ae628e421cb81d5c67d7

--- jobs/_shared/ExtraSettings.php
+++ jobs/_shared/ExtraSettings.php
@@ -10,7 +10,8 @@ $wgShowExceptionDetails = true;
 
 // gallium.wikimedia.org has tmpfs installed, use that instead of
 // the default /tmp.
-$jenkinsTmpFs = '/var/lib/jenkins/tmpfs';
+$jenkinsUserHome = getenv('HOME') ?: '/var/lib/jenkins';
+$jenkinsTmpFs = "{$jenkinsUserHome}/tmpfs";
 $jenkinsJobName = getenv( 'JOB_NAME' );
 if ( $jenkinsJobName && is_dir( "$jenkinsTmpFs/$jenkinsJobName" ) ) {
 	$wgTmpDirectory = "$jenkinsTmpFs/$jenkinsJobName";
