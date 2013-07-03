3ef3f997e766ac038a93080511b08482c840b5dc
Antoine Musso <hashar@free.fr>
2013-07-03 13:48:11 +0000

    tweaks for puppet-test-runner
    use find | xargs, no more using an intermediate variable.
    
    Change-Id: Iec0bde5976e1f297ccbfc7aeb963fba88bbeb321

--- bin/puppet-test-runner.sh
+++ bin/puppet-test-runner.sh
@@ -24,27 +24,25 @@ MAX_JOBS=4
 puppet_apply_noop() {
 	manifest="$1"
 
-	echo "info: Invoking puppet on '$1'"
-
+	echo "INFO: Invoking puppet on '$1'"
 	puppet apply --noop --modulepath="$MODULE_DIR" $manifest
 
 	test_result=$?
-	[[ $test_result != 0 ]] && echo "error: failed tests for '$manifest'"
+	[[ $test_result != 0 ]] && echo "ERROR: failed tests for '$manifest'"
 	return $test_result
 }
 
-echo "info: Will assume modules are under '$MODULE_DIR'"
-TEST_FILES=`find . -type f -wholename '*tests/*.pp'`
-
-echo "info: Triggering tests using up to $MAX_JOBS puppet runners"
+echo "INFO: Will assume modules are under '$MODULE_DIR'"
+echo "INFO: Triggering tests using up to $MAX_JOBS puppet runners"
 
 # Export environnent so that is made available to the subshell executed
 # by xargs.
 export MODULE_DIR
 export -f puppet_apply_noop
 
-echo $TEST_FILES|xargs -t -I '{}' -P$MAX_JOBS -n1 bash -c 'puppet_apply_noop {}'
+find . -type f -wholename '*tests/*.pp' -print0 \
+	| xargs -0 -t -I '{}' -P$MAX_JOBS -n1 bash -c 'puppet_apply_noop {}'
 FINAL_EXIT_CODE=$?
 
-echo "info: Final exit code: $FINAL_EXIT_CODE"
+echo "INFO: Final exit code: $FINAL_EXIT_CODE"
 exit $FINAL_EXIT_CODE
