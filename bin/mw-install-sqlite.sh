a4c4d91e9c36ca25543a75206f9cd1fbccc762ab
Antoine Musso <hashar@free.fr>
2013-10-02 22:14:30 +0200

    mw-install-sqlite.sh for Jenkins
    Originally comes from /jobs/_shared/build.xml
    
    Change-Id: I0c5a6c96dbf1ce4dc147842bb6ab12e8ae85aed5

---
+++ bin/mw-install-sqlite.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -ex
+
+# We sometime have a tmpfs to use, that speeds up sqlite
+if [ -d "$HOME/tmpfs" ]; then
+	SQLITE_DIR="$HOME/tmpfs/$JOB_NAME"
+	mkdir -p $SQLITE_DIR
+else
+	SQLITE_DIR="$WORKSPACE/data"
+fi
+
+# Ensure LocalSettings does not exist
+rm -f LocalSettings.php
+# Purge sqlite databases
+rm -f "$SQLITE_DIR/*.sqlite"
+
+# $wgDBName
+DB_NAME="build${BUILD_NUMBER}"
+
+# Run MediaWiki installer
+php maintenance/install.php \
+	--confpath "$WORKSPACE" \
+	--dbtype=sqlite \
+	--dbname="$DB_NAME" \
+	--dbpath="$SQLITE_DIR" \
+	--pass testpass \
+	sqlitetest \
+	WikiAdmin
+
+# Installer creates sqlite db as 644 jenkins:jenkins
+# Make it writable by Apache (bug 47639)
+chmod a+w "${SQLITE_DIR}/${DB_NAME}.sqlite"
