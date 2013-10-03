4edbf7a8e5e531f2357db973216f5f10e148d4e2
Antoine Musso <hashar@free.fr>
2013-10-03 11:08:28 +0200

    mw-run-phpunit-allexts.sh replaces mw-phpunit-allexts macro
    Let us more easily tweak all jobs by simply editing the shell scripts
    instead of having to refresh all dependant jobs.
    
    Change-Id: I7eb5d4d0ccbb6563a37e988a2b7ea34140dd2271

---
+++ bin/mw-run-phpunit-allexts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+# This script used to be a Jenkins Job Builder macro 'mw-phpunit-allexts'
+#
+# References:
+# bug: 42506
+# bug: 48147
+# Ib0fdffb97cdf237a49b43d7abaa81b81afe8c499
+
+JUNIT_DEST="$WORKSPACE/junit-phpunit-allexts.xml"
+
+# We have to move to the tests/phpunit directory where suite.xml is located or
+# the relative paths referenced in that file will not get properly resolved by
+# PHPUnit
+# The Jenkins publishers are usually expecting the .xml file to be at the root
+# of the workspace, so make sure we use an absolute path.
+cd "$WORKSPACE/tests/phpunit"
+
+php phpunit.php --log-junit "$JUNIT_DEST" --testsuite extensions
