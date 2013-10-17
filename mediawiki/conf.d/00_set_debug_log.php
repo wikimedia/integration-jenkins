cb0e018401b500f95c78e19dc4a60b813e9deed2
Antoine Musso <hashar@free.fr>
2013-10-17 16:39:08 +0200

    properly set $wgDebugLogFile path under Apache
    WORKSPACE is not set under Apache, fallback on $IP.
    
    Change-Id: I3959db0b1d7be25378e9345d71127729464f8e39

--- mediawiki/conf.d/00_set_debug_log.php
+++ mediawiki/conf.d/00_set_debug_log.php
@@ -7,7 +7,9 @@
 if( $wgCommandLineMode ) {
 	$wgDebugLogFile = getenv( 'WORKSPACE' ) . '/log/mw-debug-cli.log';
 } else {
-	$wgDebugLogFile = getenv( 'WORKSPACE' ) . '/log/mw-debug-www.log';
+	# Under Apache, there is no Jenkins environnement variable. Since MediaWiki
+	# include path is the workspace, use $IP instead.
+	$wgDebugLogFile = $IP . '/log/mw-debug-www.log';
 	# Make sure it is writable by Apache:
 	touch( $wgDebugLogFile );
 	chmod( $wgDebugLogFile, '0666' );
