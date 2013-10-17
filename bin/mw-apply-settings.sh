fa6a7e35856a584505782ecf16e9ff73086f1d95
Antoine Musso <hashar@free.fr>
2013-10-17 14:58:27 +0200

    mw-apply-settings: copy settings in dir log, not as a file
    Change-Id: Ibbf4ffaf8a7b430e042f271e8a8b545a564edd6b

--- bin/mw-apply-settings.sh
+++ bin/mw-apply-settings.sh
@@ -5,7 +5,7 @@ MEDIAWIKI_D="/srv/slave-scripts/mediawiki/conf.d"
 php "$MEDIAWIKI_D/_join.php" >> "$LOCAL_SETTINGS"
 
 # Copy under /log for archival purposes
-cp "$LOCAL_SETTINGS" "$WORKSPACE/log"
+cp "$LOCAL_SETTINGS" "$WORKSPACE/log/"
 
 echo "Making sure $LOCAL_SETTINGS is still valid"
 php -l "$LOCAL_SETTINGS"
