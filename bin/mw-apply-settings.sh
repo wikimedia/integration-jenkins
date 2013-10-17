6c74ee62a8e8aa62bb26abf2db96ddcf9835c2a3
Antoine Musso <hashar@free.fr>
2013-10-17 15:01:27 +0200

    mw-apply-settings: make sure log dir exist
    Change-Id: I65b5e28f7216e8c0baad96910083ff8462ce2d27

--- bin/mw-apply-settings.sh
+++ bin/mw-apply-settings.sh
@@ -2,10 +2,14 @@
 LOCAL_SETTINGS="$WORKSPACE/LocalSettings.php"
 MEDIAWIKI_D="/srv/slave-scripts/mediawiki/conf.d"
 
+# Setup Junit destination
+LOG_DIR="$WORKSPACE/log"
+
 php "$MEDIAWIKI_D/_join.php" >> "$LOCAL_SETTINGS"
 
 # Copy under /log for archival purposes
-cp "$LOCAL_SETTINGS" "$WORKSPACE/log/"
+mkdir -p "$LOG_DIR"
+cp "$LOCAL_SETTINGS" "$LOG_DIR"
 
 echo "Making sure $LOCAL_SETTINGS is still valid"
 php -l "$LOCAL_SETTINGS"
