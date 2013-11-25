46bc079bb93b7f7a0f516c392f2524e93adbff27
Antoine Musso <hashar@free.fr>
2013-11-25 16:05:52 +0100

    mw-core-get.sh: snapshot a mediawiki/core treeish
    On labs we do not have a local replication of mediawiki/core, instead
    fallback to git.wikimedia.org.
    
    Change-Id: I37ff7884df542aba23d1db57dbcdb12362e9b180

---
+++ bin/mw-core-get.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# Local bare git repository
+# On Wikimedia CI server, that is a Gerrit replica
+GIT_LOCAL="/srv/ssd/gerrit/mediawiki/core.git"
+
+# git tree-ish passed to git-archve (or gitblit)
+TREE_ISH=${1:-'master'}
+
+# Very basic validation
+if [[ ! -d "$WORKSPACE" || ! -w "$WORKSPACE" ]]; then
+	echo "\$WORKSPACE must be pointing to a directory"
+	exit 1
+fi
+
+function tar_extract() {
+	(cd "$WORKSPACE" && tar xzf -)
+}
+
+if [ -d "$GIT_LOCAL"]; then
+	git archive --remote="$GIT_LOCAL" "$TREE_ISH" | tar_extract
+else
+	# Fallback to git.wikimedia.org
+	curl "https://git.wikimedia.org/zip/?r=mediawiki/core.git&format=gz&h=$TREE_ISH" | tar_extract
+fi
