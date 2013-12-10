8b4f6c81b65495f7872c2efc812fb7d1bee2f5e9
Antoine Musso <hashar@free.fr>
2013-12-10 17:28:29 +0100

    mwconf: $wg settings for djvu
    Tpt is working on adding some djvu integration tests to mw/core in
    https://gerrit.wikimedia.org/r/#/c/98258/ we thus need to setup the djvu
    $wg variables.
    
    Change-Id: I8a9f5b1fd0db978265eb3a06ebbf764a68636bc1

---
+++ mediawiki/conf.d/20_set_wgdjvu.php
@@ -0,0 +1,11 @@
+<?php
+#
+# Snippet coming from integration/jenkins.git
+# mediawiki.d/20_set_wgdvju.php
+#
+# Used to configure $wgDjvu for the MediaWiki core DJVU tests
+
+$wgDjvuDump = '/usr/bin/djvudump';
+$wgDjvuRenderer = '/usr/bin/ddjvu';
+$wgDjvuToXml = '/usr/bin/djvutoxml';
+$wgDjvuTxt = '/usr/bin/djvutxt';
