4382a2b9cc893de0671d3fd4e610049e8c9c60a5
Antoine Musso <hashar@free.fr>
2014-01-03 17:00:00 +0100

    mwconf: set $wgHTTPProxy based on wmf site
    Some tests rely on an internet access, for example the Math extension
    fetch material from latexml. Hacking $wgHTTPProxy does fix the issue :
    https://gerrit.wikimedia.org/r/#/c/105188/
    
    Made an anonymous function to avoid polluting the global namespace.
    
    The default proxy is webproxy.eqiad.wmnet, it is changed for the pmtpa
    cluster.
    
    bug: 59253
    Change-Id: Idb8ab2909aec3ee38ab724ca5dc2b232b1a7d267

---
+++ mediawiki/conf.d/10_set_wgHTTPProxy.php
@@ -0,0 +1,24 @@
+<?php
+# Snippet coming from integration/jenkins.git
+# mediawiki.d/10_set_wgHTTPProxy.php
+#
+# Set $wgHTTPProxy depending on the site the code is being executed.
+#
+# References:
+# https://bugzilla.wikimedia.org/59253
+# https://wikitech.wikimedia.org/wiki/Http_proxy
+#
+$wgHTTPProxy = call_user_func( function() {
+	$proxy = 'webproxy.eqiad.wmnet:8080';
+	$site_file = '/etc/wikimedia-site';
+	if ( file_exists( $site_file ) ) {
+		$site = rtrim(file_get_contents('/etc/wikimedia-site'));
+		switch( $site ) {
+			case 'pmtpa':
+				$proxy = 'webproxy.pmtpa.wmnet:8080';
+			break;
+		}
+	}
+
+	return $proxy;
+} );
