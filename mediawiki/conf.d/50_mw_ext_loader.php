a73b7e252d43993a6242427eedd4e3118df61e09
Antoine Musso <hashar@free.fr>
2014-01-06 14:50:42 +0100

    ext loader now in mediawiki/conf.d
    We used to load extensions with the InjectExtensions.php script, it is
    no more needed so delete it.
    
    Injecting configuration should be done with snippets under
    mediawiki/conf.d/. The 50_mw_ext_loader.php script was merely including
    the file tools/extensions-loader.php , so just move that file there.
    
    Change-Id: I48d9975fe675149ecacaf55e8e72bdcce13b2c15

--- mediawiki/conf.d/50_mw_ext_loader.php
+++ mediawiki/conf.d/50_mw_ext_loader.php
@@ -1,8 +1,75 @@
 <?php
 #
 # Snippet coming from integration/jenkins.git
-# mediawiki.d/50_mw_ext_loader.php
+# mediawiki.d/50_mw_ext_oader.php
 #
 
-// FIXME: migrate file content there.
-require_once( '/srv/deployment/integration/slave-scripts/tools/extensions-loader.php' );
+$func_get_exts = function () {
+	global $IP;
+	if( !is_dir( $IP ) ) {
+		echo "Invalid MediaWiki path '$IP'.\n";
+		echo "Aborting\n";
+		exit(1);
+	}
+
+	# Find out the current project:
+	$ZUUL_PROJECT = getenv( 'ZUUL_PROJECT' );
+
+	# Attempt to extract the extension name:
+	$currentExt = null;
+	$m = array();
+	if( preg_match_all(
+		'%^mediawiki/extensions/(.*)$%',
+		$ZUUL_PROJECT,
+		$m )
+	) {
+		$currentExt = $m[1][0];
+	}
+
+	$ext_to_load = array();
+	$ext_missing = array();
+	foreach( scandir("{$IP}/extensions/") as $extname ) {
+		if( $extname == '.'
+			|| $extname == '..'
+			|| !is_dir( "{$IP}/extensions/${extname}" )
+		) {
+			continue;
+		}
+
+		# Bug 42960: ignore empty extensions
+		$hasContent = array_diff(
+			scandir( "{$IP}/extensions/${extname}"),
+			array('.','..')
+		);
+		if( !$hasContent ) {
+			continue;
+		}
+
+		$extfile = "{$IP}/extensions/{$extname}/{$extname}.php";
+		if( file_exists($extfile) ) {
+			$ext_to_load[$extname] = $extfile;
+		} else {
+			$ext_missing[] = $extfile;
+		}
+	}
+	if( count($ext_missing) ) {
+		echo "Could not load some extensions because they are missing\n";
+		echo "the expected entry point:\n\n";
+		echo implode( "\n", $ext_missing );
+		echo "\n\nAborting\n";
+		exit(1);
+	}
+
+	# Make sure the current extension is loaded last
+	if( $currentExt ) {
+		$currentExtFile = $ext_to_load[$currentExt];
+		unset( $ext_to_load[$currentExt] );
+		$ext_to_load[$currentExt] = $currentExtFile;
+	}
+
+	return $ext_to_load;
+};
+
+foreach( $func_get_exts() as $extname ) {
+	require_once( $extname );
+}
