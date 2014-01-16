4738d8444fce3548173c157fde469962281e67c7
Antoine Musso <hashar@free.fr>
2014-01-16 15:22:58 +0100

    Script to sync VisualEditor in mediawiki/extensions.git
    bug: 49846
    Change-Id: I092e08b8b6e1907f1e9e141a5de378bb610f7e36

---
+++ bin/gerrit-sync-ve.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -xe
+#
+# Script to update VisualEditor extension in mediawiki/extensions.git
+#
+# The script interacts with Gerrit using the jenkins-bot credentials which
+# belong to the user jenkins on gallium.  The Jenkins job invoking this script
+# must be tied to gallium and the jenkins-slave needs a sudo policy to be able
+# to run this script as the jenkins user (to access jenkins-bot ssh
+# credentials).
+#
+# The script should be run via a job triggered in the Zuul 'postmerge'
+# pipeline. Zuul will provide the merged commit in the ZUUL_COMMIT parameter
+# which is exposed in the Jenkins job as the $ZUUL_COMMIT env variable.
+
+MWEXT_DIR='src/extensions'
+GERRIT_USER='jenkins-bot'
+MWEXT_REPO="ssh://${GERRIT_USER}@gerrit.wikimedia.org:29418/mediawiki/extensions.git"
+
+if [[ -z "${ZUUL_COMMIT}" ]]; then
+	echo "\$ZUUL_COMMIT must be set!"
+	exit 1
+fi
+
+if [[ "$USER" != "jenkins" ]]; then
+	echo "Must be run as 'jenkins' user to access jenkins-bot SSH credentials"
+	exit 1
+fi
+
+# Initialize mediawiki/extensions checkout with no submodules
+echo
+mkdir -p "${MWEXT_DIR}"
+cd "${MWEXT_DIR}"
+git init
+git remote add origin "${MWEXT_REPO}" ||
+	git remote set-url origin "${MWEXT_REPO}"
+
+# Install commit hook if needed
+if [ ! -e ".git/hooks/commit-msg" ]; then
+	scp -p -P 29418 "${GERRIT_USER}"@gerrit.wikimedia.org:hooks/commit-msg `git rev-parse --git-dir`/hooks/commit-msg
+fi
+
+git fetch --recurse-submodules=no --verbose origin
+git reset --hard origin/master
+
+# Initialize VisualEditor submodule
+echo
+pwd
+git submodule update --init VisualEditor
+cd VisualEditor
+VE_OLD_SHORT=`git rev-parse --short HEAD`
+VE_OLD_LONG=`git rev-parse HEAD`
+if [[ ${VE_OLD_LONG} == ${ZUUL_COMMIT} ]]; then
+	echo "VisualEditor is already pointing to Zuul commit version:"
+	echo "HEAD.........: ${VE_OLD_LONG}"
+	echo "ZUUL_COMMIT..: ${ZUUL_COMMIT}"
+	exit 1
+fi
+git fetch origin
+git reset --hard "${ZUUL_COMMIT}"
+ZUUL_COMMIT_SHORT=`git rev-parse --short HEAD`
+git show --no-patch --format=fuller --color
+cd ..
+
+# Add change to Gerrit
+echo
+pwd
+git add VisualEditor
+git commit -a -m "Syncronize VisualEditor: ${VE_OLD_SHORT}..${ZUUL_COMMIT_SHORT}"
+git show
+git push origin HEAD:refs/for/master
+MWEXT_HEAD=`git rev-parse HEAD`
+ssh -p 29418 "${GERRIT_USER}"@gerrit.wikimedia.org "gerrit approve --code-review +2 --verified +2 --submit $MWEXT_HEAD"
