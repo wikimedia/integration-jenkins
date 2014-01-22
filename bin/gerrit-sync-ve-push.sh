06a1d2912f4f5ee88c7ab7fb42b96398d5041de7
Antoine Musso <hashar@free.fr>
2014-01-21 17:13:34 -0800

    gerrit-sync-ve: switch to jenkins-mwext-sync user
    Use a new Gerrit user to commit the updates: jenkins-mwext-sync.  This
    way we can remove the email address of jenkins-bot user to prevent the
    email recipients to be spammed by all Gerrit activities.
    
    Change-Id: Ie1415db5bef8ae9ea2c4b19cf12b82830902b223

--- bin/gerrit-sync-ve-push.sh
+++ bin/gerrit-sync-ve-push.sh
@@ -3,8 +3,9 @@
 # Script to push VisualEditor updates to mediawiki/extensions.git
 
 MWEXT_DIR='src/extensions'
-GERRIT_USER='jenkins-bot'
+GERRIT_USER='jenkins-mwext-sync'
 MWEXT_REPO_SSH="ssh://${GERRIT_USER}@gerrit.wikimedia.org:29418/mediawiki/extensions.git"
+GERRIT_USER_SSH_IDENTITY="/var/lib/jenkins/.ssh/jenkins-mwext-sync_id_rsa"
 
 if [[ "$USER" != "jenkins" ]]; then
 	echo "Must be run as 'jenkins' user to access $GERRIT_USER SSH credentials"
@@ -18,4 +19,5 @@ git show
 # since files in the workspace belong to jenkins-slave user.
 git push "$MWEXT_REPO_SSH" HEAD:refs/for/master
 MWEXT_HEAD=`git rev-parse HEAD`
-ssh -p 29418 "${GERRIT_USER}"@gerrit.wikimedia.org "gerrit approve --code-review +2 --verified +2 --submit $MWEXT_HEAD"
+ssh -i "$GERRIT_USER_SSH_IDENTITY" -p 29418 \
+	"${GERRIT_USER}"@gerrit.wikimedia.org "gerrit approve --code-review +2 --verified +2 --submit $MWEXT_HEAD"
