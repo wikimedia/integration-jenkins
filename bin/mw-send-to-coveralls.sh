07147ea839c71b934dbd7649bbc61e97b76456ca
addshore <addshorewiki@gmail.com>
2014-02-28 00:02:15 +0100

    Create symlink to vendor if non exists
    Change-Id: I87b039992dfda5b8b3ba729ae05d261838e07616

--- bin/mw-send-to-coveralls.sh
+++ bin/mw-send-to-coveralls.sh
@@ -2,6 +2,12 @@
 
 rm -f .coveralls.yml
 
+# As coveralls was meant to be installed using composer we need
+# to trick it into thinking that it was by adding this symlink
+if [ ! -L vendor ]; then
+  ln -s /srv/deployment/integration/php-coveralls/vendor vendor
+fi
+
 echo "# .coveralls.yml generated my jenkins.git\bin\mw-send-to-coveralls.sh" >> .coveralls.yml
 set +x
 echo "repo_token: $(cat /var/lib/jenkins-slave/coveralls.io.token)" >> .coveralls.yml
@@ -12,4 +18,4 @@ echo "service_event_type: manual" >> .coveralls.yml
 echo "coverage_clover: log/clover.xml" >> .coveralls.yml
 echo "json_path: log/coveralls-upload.json" >> .coveralls.yml
 
-php /srv/deployment/integration/php-coveralls/vendor/bin/coveralls
+php vendor/bin/coveralls
