1a517d762c1de57fbfbb2e17fc4c47852c82fffc
addshore <addshorewiki@gmail.com>
2014-02-28 12:44:04 +0100

    Trick coveralls into thinking we installed using composer
    As coveralls uses realpath and other intreseting
      things such as PHP_COVERALLS_COMPOSER_INSTALL
      it will only correctly work if it is exactly as it
      would be if it was installed by composer.
    
    Thus we need to copy the vendor dir over rather
      than using a symlink.
    Naturally not the tidiest solution (that would be
      using composer) but we cant do that here
    
    Change-Id: Ief7d2329fc9d14ca3d53038cd3560040e7c84aca

--- bin/mw-send-to-coveralls.sh
+++ bin/mw-send-to-coveralls.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -xe
 
+# Cleanup
 rm -f .coveralls.yml
+rm -rf vendor
 
-# As coveralls was meant to be installed using composer we need
-# to trick it into thinking that it was by adding this symlink
-if [ ! -L vendor ]; then
-  ln -s /srv/deployment/integration/php-coveralls/vendor vendor
-fi
+# Pretend we have installed php-coveralls using composer
+cp /srv/deployment/integration/php-coveralls/vendor vendor
 
+# Create the .coveralls.yml file
 echo "# .coveralls.yml generated my jenkins.git\bin\mw-send-to-coveralls.sh" >> .coveralls.yml
 set +x
 echo "repo_token: $(cat /var/lib/jenkins-slave/coveralls.io.token)" >> .coveralls.yml
@@ -18,4 +18,5 @@ echo "service_event_type: manual" >> .coveralls.yml
 echo "coverage_clover: log/clover.xml" >> .coveralls.yml
 echo "json_path: log/coveralls-upload.json" >> .coveralls.yml
 
+# Actually run coveralls!
 php vendor/bin/coveralls
