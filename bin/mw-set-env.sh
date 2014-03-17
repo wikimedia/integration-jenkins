ebe53a269d47fc009037d1d0ed3929ea8a90e346
Faidon Liambotis <faidon@wikimedia.org>
2014-03-18 01:39:44 +0200

    Dump corefiles on segfaults
    When e.g. phpunit crashes, it's useful to have a corefile for postmortem
    analysis, as sometimes crashes are hard to reproduce outside the
    environment or when trying to catch heisenbugs.
    
    Set a 64MB upper ulimit (it should suffice to get started with) and set
    it globally on the included mw-set-env.sh script to be included by all
    run scripts.
    
    Bug: 62623
    Change-Id: I7a79e8e02123251732f4c729e00928f9f55ae9a3

--- bin/mw-set-env.sh
+++ bin/mw-set-env.sh
@@ -3,6 +3,9 @@
 # Script to set up various environement variables suitable to test out
 # MediaWiki core and extensions on Wikimedia continuous integration platform.
 
+# allow the dumping of corefiles, up to 64MB
+ulimit -c 64000
+
 MW_INSTALL_PATH="$WORKSPACE/src/mediawiki/core"
 if [[ ! -d "$MW_INSTALL_PATH" ]]; then
 	MW_INSTALL_PATH="$WORKSPACE"
