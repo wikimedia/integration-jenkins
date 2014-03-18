c9cba76284b377b96fcd12632fca7fcb2c6770d1
Hashar <hashar@free.fr>
2014-03-18 14:12:45 +0000

    Revert "mwconf: route all web proxy traffic to pmtpa"
    The proxy in eqiad is back up!
    
    RT #7069
    
    This reverts commit a81aee0db4ecea5481b1cb3a73af91183114b91a.
    
    Change-Id: I173197fcaeaf54d5bcbd1bd6b751b1d281e02d6b

--- mediawiki/conf.d/10_set_wgHTTPProxy.php
+++ mediawiki/conf.d/10_set_wgHTTPProxy.php
@@ -9,11 +9,7 @@
 # https://wikitech.wikimedia.org/wiki/Http_proxy
 #
 $wgHTTPProxy = call_user_func( function() {
-	# Carbon is dead RT #7069 - hashar 20140318
-	# Route everything to pmtpa
-	#$proxy = 'webproxy.eqiad.wmnet:8080';
-	$proxy = 'webproxy.pmtpa.wmnet:8080';
-
+	$proxy = 'webproxy.eqiad.wmnet:8080';
 	$site_file = '/etc/wikimedia-site';
 	if ( file_exists( $site_file ) ) {
 		$site = rtrim(file_get_contents('/etc/wikimedia-site'));
