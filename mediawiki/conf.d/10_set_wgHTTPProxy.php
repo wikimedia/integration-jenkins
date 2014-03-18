a81aee0db4ecea5481b1cb3a73af91183114b91a
Antoine Musso <hashar@free.fr>
2014-03-18 14:16:01 +0100

    mwconf: route all web proxy traffic to pmtpa
    The eqiad proxy carbon is being reinstalled since March 14th. Tweak the
    configuration to point to webproxy.PMTPA.wmnet as a workaround.
    
    RT #7069
    
    Change-Id: I7a9bf3fce3a8a8c0cf97387e3ab655b2974118bd

--- mediawiki/conf.d/10_set_wgHTTPProxy.php
+++ mediawiki/conf.d/10_set_wgHTTPProxy.php
@@ -9,7 +9,11 @@
 # https://wikitech.wikimedia.org/wiki/Http_proxy
 #
 $wgHTTPProxy = call_user_func( function() {
-	$proxy = 'webproxy.eqiad.wmnet:8080';
+	# Carbon is dead RT #7069 - hashar 20140318
+	# Route everything to pmtpa
+	#$proxy = 'webproxy.eqiad.wmnet:8080';
+	$proxy = 'webproxy.pmtpa.wmnet:8080';
+
 	$site_file = '/etc/wikimedia-site';
 	if ( file_exists( $site_file ) ) {
 		$site = rtrim(file_get_contents('/etc/wikimedia-site'));
