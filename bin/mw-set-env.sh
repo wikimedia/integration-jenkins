ac377a77fd10277a6e49de02d1309154bc91df50
Faidon Liambotis <faidon@wikimedia.org>
2014-03-24 23:00:44 +0200

    Bump ulimit max size for corefiles
    The corefile generated just now for the PHPUnit segfault says
    BFD: Warning: /home/hashar/bug62623.core is truncated: expected core
    file size >= 651972608, found: 65609728.
    ...so, clearly, 64MB was overoptimistic. Bump this to an arbitrary 2GB
    limit, but this means to be way more aggressive on our clean ups (e.g. a
    day, instead of a week).
    
    Change-Id: Ic4a6397ab8bb1592d0346ea04be4f756651dc84e

--- bin/mw-set-env.sh
+++ bin/mw-set-env.sh
@@ -3,8 +3,8 @@
 # Script to set up various environement variables suitable to test out
 # MediaWiki core and extensions on Wikimedia continuous integration platform.
 
-# allow the dumping of corefiles, up to 64MB
-ulimit -c 64000
+# allow the dumping of corefiles, up to 2GB
+ulimit -c 2097152
 
 MW_INSTALL_PATH="$WORKSPACE/src/mediawiki/core"
 if [[ ! -d "$MW_INSTALL_PATH" ]]; then
