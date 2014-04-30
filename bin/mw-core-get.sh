e59725914253ec26312de41d94008bbb7206a828
C. Scott Ananian <cscott@cscott.net>
2014-04-30 15:48:44 -0400

    mw-core-get.sh: install to $MW_INSTALL_PATH instead of $WORKSPACE.
    Change-Id: Id22cd337741b24396c4b91acbcc4c9288a04388f

--- bin/mw-core-get.sh
+++ bin/mw-core-get.sh
@@ -7,9 +7,12 @@ GIT_LOCAL="/srv/ssd/gerrit/mediawiki/core.git"
 # git tree-ish passed to git-archve (or gitblit)
 TREE_ISH=${1:-'master'}
 
+# Set MW_INSTALL_PATH
+. "/srv/deployment/integration/slave-scripts/bin/mw-set-env.sh"
+
 # Very basic validation
-if [[ ! -d "$WORKSPACE" || ! -w "$WORKSPACE" ]]; then
-	echo "\$WORKSPACE must be pointing to a directory"
+if [[ ! -d "$MW_INSTALL_PATH" || ! -w "$MW_INSTALL_PATH" ]]; then
+	echo "\$MW_INSTALL_PATH must be pointing to a directory"
 	exit 1
 fi
 
@@ -17,9 +20,9 @@ if [ -d "$GIT_LOCAL" ]; then
 	# Record the exact commit fetched on stderr via 'git get-tar-commit-id'
 	git archive --remote="$GIT_LOCAL" "$TREE_ISH" \
 		| tee >(git get-tar-commit-id 1>&2 ; cat > /dev/null ) \
-		| (cd "$WORKSPACE" && tar xf -)
+		| (cd "$MW_INSTALL_PATH" && tar xf -)
 else
 	# Fallback to git.wikimedia.org
 	curl "https://git.wikimedia.org/zip/?r=mediawiki/core.git&format=gz&h=$TREE_ISH" \
-		| (cd "$WORKSPACE" && tar xzf -)
+		| (cd "$MW_INSTALL_PATH" && tar xzf -)
 fi
