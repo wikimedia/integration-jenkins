e8c9189117a851a39f203d86e88601a6368d2c6a
Antoine Musso <hashar@free.fr>
2014-06-03 09:41:12 +0200

    mw-set-env: set mediawiki to /src if exists
    The lookup order for MW_INSTALL_PATH is now:
    
     src/mediawiki/core
     src
     .
    
    Change-Id: I93771f60c8eb14921b6d5af2db5ce3e99df1709c

--- bin/mw-set-env.sh
+++ bin/mw-set-env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # Script to set up various environement variables suitable to test out
 # MediaWiki core and extensions on Wikimedia continuous integration platform.
@@ -6,8 +6,11 @@
 # allow the dumping of corefiles, up to 2GB
 ulimit -c 2097152
 
-MW_INSTALL_PATH="$WORKSPACE/src/mediawiki/core"
-if [[ ! -d "$MW_INSTALL_PATH" ]]; then
-	MW_INSTALL_PATH="$WORKSPACE"
-fi
+MW_INSTALL_PATH=$WORKSPACE
+for mw_path in src/mediawiki/core src; do
+	if [[ -d "$WORKSPACE/$mw_path" ]]; then
+		MW_INSTALL_PATH="$WORKSPACE/$mw_path"
+		break
+	fi;
+done;
 export MW_INSTALL_PATH
