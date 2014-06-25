282948a8c967e1af33937974b3d6eba6c8f3c07b
Antoine Musso <hashar@free.fr>
2014-06-25 22:08:34 +0200

    Sleep one second between update.php runs
    update.php --schema insert an entry in updatelog such as:
     updatelist-1.24alpha-1403726256
    
    When running update.php without --schema, it attempts to use the same
    key if it started during the same second.  That causes a duplicate key:
    Error: 19 column ul_key is not unique
    
    Sleeping one second is lame but workaround the issue.
    
    Change-Id: I4c09530fc81fccf2368b3ca7e795a29c34d421f3

--- bin/mw-run-update-script.sh
+++ bin/mw-run-update-script.sh
@@ -14,5 +14,14 @@ $UPDATE --quiet --quick --schema $LOG_FILE
 # delete unless it has some content
 [ -s $LOG_FILE ] || rm $LOG_FILE
 
+# Running --schema insert an entry in `updatelog` table which vary by unix
+# epoch.  When rerunning the update.php script without --schema, we end up with
+# a duplicate key for ul_key.
+# Sleeping for one second will ensure the second call of update.php has a
+# different unix timestamp and hence a different key.
+#
+# -- hashar 2014-06-25
+sleep 1
+
 # Actually run the update
 $UPDATE --quick
