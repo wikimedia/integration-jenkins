be7941bea28a377a0801ab987a82c8c13ab45ead
Antoine Musso <hashar@free.fr>
2014-08-05 14:21:14 +0200

    civicrm: revoke prileges
    Change-Id: I05f8a40e52358c95e45b6b43b6e4f5725f950cba

--- bin/civi-ci-drop-dbs.sh
+++ bin/civi-ci-drop-dbs.sh
@@ -7,4 +7,6 @@ echo "Dropping databases with the suffix -'_${JOB_ID}'"
 mysql -u root <<EOS
 drop database if exists ${DRUPAL_SCHEMA};
 drop database if exists ${CIVICRM_SCHEMA};
+
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM ${CIVICRM_MYSQL_USERNAME}@${BUILD_HOST};
 EOS
