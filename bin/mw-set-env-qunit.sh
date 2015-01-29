6fb21575035c922c5ed4d4d7ecc7af15c26e544d
Timo Tijhof <krinklemail@gmail.com>
2015-01-29 12:43:05 -0800

    Touch mw-set-env-qunit.sh to test T87843
    Remove tmp.sh (reverts 64c07be).
    
    Change-Id: Ib64df9a69b822cce7b89e564146bfe56dc13b4ac

--- bin/mw-set-env-qunit.sh
+++ bin/mw-set-env-qunit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-. "/srv/deployment/integration/slave-scripts/bin/mw-set-env.sh"
+. /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
 export MW_SERVER="http://localhost:9412"
 export MW_SCRIPT_PATH="/$BUILD_TAG"
