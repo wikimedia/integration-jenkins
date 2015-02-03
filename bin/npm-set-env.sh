28f2ca8924c20ebb43a085723c86dd43afde8356
Timo Tijhof <krinklemail@gmail.com>
2015-02-02 19:41:36 -0800

    npm-set-env.sh: Purge node_modules
    Bug: T88395
    Bug: T76304
    Change-Id: I8a52fd3bd1181b8d4d181abaf24bea4c96131301

--- bin/npm-set-env.sh
+++ bin/npm-set-env.sh
@@ -2,6 +2,9 @@
 #
 # Script to set custom environement variables for 'npm test' jobs.
 #
+# Must be run in the directory containing package.json.
+# For jobs that manually 'cd' into such directory, don't run it too early!
+#
 
 # Integration slaves have an Xvfb window with server number 94 reserved for
 # local tests.
@@ -10,3 +13,6 @@ export DISPLAY=':94'
 # Set CHROME_BIN for projects using karma-chrome-launcher as our slaves have
 # Chromium instead of Chrome.
 export CHROME_BIN=`which chromium-browser`
+
+# Purge any existing node_modules. (T88395, T76304)
+rm -rf node_modules
