afc1ef05ff1455ea3ddebd0a8cd39c3a4bf67ed3
Timo Tijhof <krinklemail@gmail.com>
2015-03-20 07:43:27 +0000

    Use EXECUTOR_NUMBER instead of BUILD_TAG in TMPDIR
    Because.. UNIX socket paths can't be longer than 100 characters.
    
    Follows-up bb2bf9e, which broke this by including BUILD_TAG
    which added the substring "jenkins" and pushed it over the edge.
    
    Bug: T93330
    Change-Id: Ibb89790fbdd011ca84c08ee412ffd1576de93e17

--- bin/mw-set-env.sh
+++ bin/mw-set-env.sh
@@ -18,7 +18,9 @@ export MW_INSTALL_PATH
 # All slaves should have tmpfs mounted, use if available
 if [ -d "$HOME/tmpfs" ]; then
 	# Don't use JOB_NAME since that is not unique when running concurrent builds (T91070).
-	export TMPDIR="$HOME/tmpfs/$BUILD_TAG"
+	# Don't use a path longer than 55 characters in total. Chromium needs 45 characters
+	# for its user-data directory and socket. 103 is the Unix maxlength for socket paths (T93330).
+	export TMPDIR="$HOME/tmpfs/jenkins-${EXECUTOR_NUMBER}"
 	export MW_TMPDIR="$TMPDIR"
 else
 	export MW_TMPDIR="$WORKSPACE/data"
