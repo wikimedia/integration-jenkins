aefcc615de112d6474cc97b5a8311dcae695e92e
Timo Tijhof <krinklemail@gmail.com>
2015-03-24 03:18:50 +0000

    mw-teardown: Include mw-set-env
    While this script has become a placeholder after recent cleanup,
    the mw-teardown-mysql does extend it and needs the env vars to be there.
    
    Change-Id: I9a08d11e7381cfdbcace01218d67c3c54a23dc8e

--- bin/mw-teardown.sh
+++ bin/mw-teardown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
 . /srv/deployment/integration/slave-scripts/bin/global-teardown.sh
+. /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
-# . /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 # Placeholder
