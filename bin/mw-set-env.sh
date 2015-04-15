131332712d98501481d4c89df073071ea1580231
Timo Tijhof <krinklemail@gmail.com>
2015-04-15 23:12:02 +0100

    mw-set-env: Remove core dump file size allowance of 2GB
    It's 0 by default.
    
    Bug: T96025
    Change-Id: Ic82ba43bd29673a76afccde310bcc79cb1d5f71b

--- bin/mw-set-env.sh
+++ bin/mw-set-env.sh
@@ -5,9 +5,6 @@
 # Script to set up various environement variables suitable to test out
 # MediaWiki core and extensions on Wikimedia continuous integration platform.
 
-# allow the dumping of corefiles, up to 2GB
-ulimit -c 2097152
-
 MW_INSTALL_PATH=$WORKSPACE
 for mw_path in src/mediawiki/core src; do
 	if [[ -d "$WORKSPACE/$mw_path" ]]; then
