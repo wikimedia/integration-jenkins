b5db3333bc39e328ba6989dbec98f3d01ad08322
Timo Tijhof <krinklemail@gmail.com>
2015-04-24 19:33:04 +0100

    Copy LocalSettings.php to "/log" in teardown instead of setup
    This way other modifications after mw-apply-settings, such as
    mw-apply-wb-settings and macro/qunit, are also reflected in the
    file collected by Jenkins build artefacts.
    
    This would've helped figure out where the syntax error was in
    the Wikibase build last month. The file uploaded to Jenkins was
    from before the error was appended.
    
    Bug: T90613
    Change-Id: I52f81eae4d795ca03ccb0882fd012be1841ad061

--- bin/mw-apply-settings.sh
+++ bin/mw-apply-settings.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
@@ -6,9 +6,6 @@ LOCAL_SETTINGS="${MW_INSTALL_PATH}/LocalSettings.php"
 MEDIAWIKI_D="/srv/deployment/integration/slave-scripts/mediawiki/conf.d"
 
 php "$MEDIAWIKI_D/_join.php" >> "$LOCAL_SETTINGS"
-
-# Copy LocalSettings under /log for archival purposes
-cp "$LOCAL_SETTINGS" "$LOG_DIR"
 
 echo "Making sure $LOCAL_SETTINGS is still valid"
 php -l "$LOCAL_SETTINGS"
--- bin/mw-teardown.sh
+++ bin/mw-teardown.sh
@@ -3,4 +3,5 @@
 . /srv/deployment/integration/slave-scripts/bin/global-teardown.sh
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
-# Placeholder
+# Copy LocalSettings to /log so that archive-log-dir collects it
+cp "${MW_INSTALL_PATH}/LocalSettings.php" "$LOG_DIR"
