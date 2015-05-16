685ab23f26cf10b4835e4402c5d1b1ce1eb2e718
Kunal Mehta <legoktm@gmail.com>
2015-05-15 17:35:06 -0700

    Run update.php with --skip-external-dependencies
    When trying to update a library in mediawiki/core and mediawiki/vendor,
    a circular dependency is produced as both patches depend upon each other.
    
    Now, all non-mediawiki/vendor jobs will skip checking for matching versions
    and continue "at their own risk". mediawiki/vendor will still check
    versions to make sure it stays in sync with MediaWiki core.
    
    Depends upon Ie84dd6320e in mediawiki/core (merged).
    
    Bug: T88211
    Change-Id: I39531a2540419ccb803e22ced7672981b42317ea

--- bin/mw-run-update-script.sh
+++ bin/mw-run-update-script.sh
@@ -2,4 +2,10 @@
 
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
-php "$MW_INSTALL_PATH/maintenance/update.php" --quick
+ARGS="--quick"
+if [ "$ZUUL_PROJECT" = "mediawiki/vendor" ]
+then
+	ARGS="$ARGS --skip-external-dependencies"
+fi
+php "$MW_INSTALL_PATH/maintenance/update.php" $ARGS
+
