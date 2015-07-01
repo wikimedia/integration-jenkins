e4f798d605d61679ea13556f50cbdf1d016e74ac
Dan Duvall <dduvall@wikimedia.org>
2015-07-01 13:43:56 -0700

    Fix headless setup for MW-Selenium
    There are a few bugs we have to workaround for now.
    
    The first is a bug in the headless gem which prevents us from reusing
    the existing Xvfb display so we'll start up Xvfb on an isolated display
    for each build. This may actually be a preferable long-term setup in
    case we need to take screen caps, etc.
    
    https://github.com/leonid-shevtsov/headless/pull/60
    
    Second is a bug in xorg on Ubuntu that prevents us from using a TMPDIR
    on a second filesystem, so we'll force use of /tmp for now.
    
    https://bugs.launchpad.net/ubuntu/+source/xorg-server/+bug/972324
    
    Change-Id: Iaf3287cfb3fe897de6fb4c4b79784a5e7ef900a8

--- bin/mw-set-env-mw-selenium.sh
+++ bin/mw-set-env-mw-selenium.sh
@@ -2,9 +2,16 @@
 
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env-localhost.sh
 
+# Let MW-Selenium setup/teardown an isolated Xvfb on a display between 70-90
+# for this build
 export HEADLESS=true
-export HEADLESS_DISPLAY="${DISPLAY##*:}"
-export HEADLESS_DESTROY_AT_EXIT=false
+export HEADLESS_DISPLAY=$((70 + EXECUTOR_NUMBER % 20))
+export HEADLESS_DESTROY_AT_EXIT=true
+
+# Xvfb barfs when trying to move xkb files across filesystems, so we must use
+# /tmp for this job
+# https://bugs.launchpad.net/ubuntu/+source/xorg-server/+bug/972324
+export TMPDIR="/tmp/jenkins-${EXECUTOR_NUMBER}"
 
 export MEDIAWIKI_ENVIRONMENT=integration
 export MEDIAWIKI_URL="${MW_SERVER}${MW_SCRIPT_PATH}/index.php"
