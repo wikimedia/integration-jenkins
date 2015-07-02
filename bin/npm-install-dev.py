d4a3db3f321f7c79430a71d8c4de607835497761
Marko Obrovac <mobrovac@wikimedia.org>
2015-07-02 10:49:51 +0200

    Add script which installs the devDependencies for Node.js services
    Normally, in our deploy repositories we don't have the dependencies used
    during development (such as test modules). This script allows their
    installation, by consulting the package.json file in the current
    directory, extracting the information about the devDeps and installs
    each of them using their given versions.
    
    Change-Id: Ie18cf994c6a1dc8b62ba2460cedb5b3dde0abb1c

---
+++ bin/npm-install-dev.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+# npm-install-dev.py
+# ------------------
+#
+# Installs the development dependencies for Node.JS services. To use it,
+# simply start it in the directory where your package.json file is
+# located. Note that it assumes the production node module dependencies
+# have already been installed, i.e. it does not check they are present.
+
+
+import json
+import subprocess
+
+
+deps = dict()
+
+print '[*] NPM devDependencies Installation [*]'
+
+with open('./package.json') as fd:
+    pkg_info = json.load(fd)
+    if 'devDependencies' in pkg_info:
+        deps = pkg_info['devDependencies']
+
+for pkg in iter(deps):
+    print 'Installing', pkg, '...'
+    subprocess.check_output(['npm', 'install', pkg + '@' + deps[pkg]])
+
+print '[*] NPM devDependencies Done [*]'
