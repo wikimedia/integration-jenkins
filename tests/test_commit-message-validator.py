7951fcfcccf9a0fbe9876f53b9b6a6c910a87fd5
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-09-30 14:05:11 +0000

    [IMPROV] Use generic commit message validation tests
    Instead of manually adding each commit message test pair, it now searches
    automatically for the pairs, so it's easier to add new ones.
    
    Change-Id: Ie1a5163f1d007634a7f4636748aa17d69a3a91c7

--- tests/test_commit-message-validator.py
+++ tests/test_commit-message-validator.py
@@ -2,6 +2,8 @@
 
 import imp
 import os
+import StringIO
+import sys
 import unittest
 
 cmv = imp.load_source(
@@ -10,29 +12,47 @@ cmv = imp.load_source(
     '/tools/commit-message-validator.py')
 
 
+class MetaValidator(type):
+
+    """
+    Create test class which checks the output for a message.
+
+    It searches through `tests/test_commit-message-validator/` and loads all
+    file pairs where there is a `.msg` and `.out` file. If both exist it'll
+    create a test for this pair where the `.msg` is the commit message and the
+    `.out` is the expected output.
+    """
+
+    def __new__(cls, name, bases, dct):
+        def create_test_method(msg, expected):
+            def test(self):
+                saved_stdout = sys.stdout
+                try:
+                    out = StringIO.StringIO()
+                    sys.stdout = out
+                    self.assertEqual(
+                        1 if expected else 0,
+                        cmv.check_message(msg.splitlines()))
+                    self.assertEqual(expected, out.getvalue())
+                finally:
+                    sys.stdout = saved_stdout
+            return test
+
+        base_path = os.path.join(os.path.dirname(__file__),
+                                 'test_commit-message-validator')
+        for fn in os.listdir(base_path):
+            test, _, extension = fn.rpartition('.')
+            fn = os.path.join(base_path, test)
+            if extension == 'msg' and os.path.isfile(fn + '.out'):
+                with open(fn + '.msg') as msg:
+                    with open(fn + '.out') as out:
+                        dct['test_' + test] = create_test_method(
+                            msg.read(), out.read())
+        return super(MetaValidator, cls).__new__(cls, name, bases, dct)
+
+
 class TestCommitMessageValidator(unittest.TestCase):
-    def test_check_message_errors(self):
-        path = os.path.dirname(__file__) + '/test_commit-message-validator/'
-        with open(path + 'check_message_errors.msg') as msg:
-            with open(path + 'check_message_errors.out') as out:
-                self._test_check_message(msg.read(), 1, out.read())
 
-    def test_check_message_ok(self):
-        path = os.path.dirname(__file__) + '/test_commit-message-validator/'
-        with open(path + 'check_message_ok.msg') as msg:
-            with open(path + 'check_message_ok.out') as out:
-                self._test_check_message(msg.read(), 0, out.read())
+    """Validate the commit messages using test files."""
 
-    def _test_check_message(self, msg, expect_status, expect_out):
-        import sys
-        import StringIO
-        saved_stdout = sys.stdout
-        try:
-            out = StringIO.StringIO()
-            sys.stdout = out
-            self.assertEqual(
-                expect_status,
-                cmv.check_message(msg.splitlines()))
-            self.assertEqual(expect_out, out.getvalue())
-        finally:
-            sys.stdout = saved_stdout
+    __metaclass__ = MetaValidator
