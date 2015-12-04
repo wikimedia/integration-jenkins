ad214bf4de6a62ddc60e1e230c18d204b513dd6a
Antoine Musso <hashar@free.fr>
2015-12-04 11:25:26 +0100

    mw-apply-settings: set $wgTmpDirectory explicitly
    When MediaWiki is hit via Apache, the env variable TMPDIR is not set and
    $wgTmpDirectory and others ends up to fall back to /tmp.  The
    localisation cache would eventually use that and since it is not garbage
    collected between build that might cause side effects on subsequent
    builds.
    
    MW_TMPDIR is set via mw-set-env / global-set-env. Eval the value to set
    explicitly set $wgTmpDirectory.
    
    Bug: T120356
    Change-Id: I6b852c5d8ab29f64ec3718dd9a659af7eb5c6806

--- bin/mw-apply-settings.sh
+++ bin/mw-apply-settings.sh
@@ -5,6 +5,14 @@
 LOCAL_SETTINGS="${MW_INSTALL_PATH}/LocalSettings.php"
 MEDIAWIKI_D="/srv/deployment/integration/slave-scripts/mediawiki/conf.d"
 
+# Apache lacks env variables, and we need MediaWiki to point to the proper
+# setup for example for the localisation cache.
+#
+# Inject the variable set via mw-set-env / global-set-env T120356
+#
+echo "Setting \$wgTmpDirectory = '${MW_TMPDIR}';"
+echo -en "<?php\n\$wgTmpDirectory = '${MW_TMPDIR}';\n?>" >> "$LOCAL_SETTINGS"
+
 php "$MEDIAWIKI_D/_join.php" >> "$LOCAL_SETTINGS"
 
 echo "Making sure $LOCAL_SETTINGS is still valid"
