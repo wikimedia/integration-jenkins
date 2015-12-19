56494440f7b363e1fed370c2abe6ffc4953eff8f
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2015-12-19 04:30:51 +0100

    Workaround for tmpdir being created with a different user
    by not removing the directory itself. Otherwise it might get created again by
    hhvm which gets a lagging request after cleanup is done.
    
    Bug: T120824
    Change-Id: I7803395f7f1774d501b60df42e2d7b6628352738

--- bin/global-teardown.sh
+++ bin/global-teardown.sh
@@ -5,5 +5,13 @@
 stat "${TMPDIR_FS}" || :
 stat "${TMPDIR_REGULAR}" || :
 
-rm -v -rf "${TMPDIR_FS}"
-rm -v -rf "${TMPDIR_REGULAR}"
+# Only delete files, leaving the directory due to a race condition with
+# Apache/HHVM threads that might still be running after the job is completed.
+# Ends up re creating the directory as owned by www-data which causes
+# jenkins-deploy to not be able to tweak the dir on subsequent runs.
+# See: T120824
+
+# Have bash '*' to expand dot files as well
+shopt -s dotglob
+rm -v -rf "${TMPDIR_FS}/*"
+rm -v -rf "${TMPDIR_REGULAR}/*"
--- bin/mw-selenium-teardown.sh
+++ bin/mw-selenium-teardown.sh
@@ -2,4 +2,7 @@
 
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env-mw-selenium.sh
 
-rm -rf "$TMPDIR"
+# Have bash '*' to expand dot files as well.
+# We keep the directory see bin/global-teardown.sh and T120824
+shopt -s dotglob
+rm -v -rf "$TMPDIR/*"
