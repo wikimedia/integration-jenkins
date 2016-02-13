4e893e53df22d590b198f9697820b65c58a70faa
Timo Tijhof <krinklemail@gmail.com>
2016-02-13 21:53:17 +0000

    Remove default PHPUNIT_DIR mw-phpunit.sh and mw-run-phpunit-allexts.sh
    This requires the MediaWiki workspaces to first have a vendor
    directory that includes dev packages (such as phpunit).
    
    The override is still supported for mw-phpunit.sh (used by a job
    for integration/phpunit.git, keeping for now).
    
    Bug: T99982
    Bug: T112895
    Change-Id: I038056178e4a6bc25158a1d1c18eb03a27294948

--- bin/mw-phpunit.sh
+++ bin/mw-phpunit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eu
 #
 # Wrapper around MediaWiki PHPUnit entry point with env based configuration
 #
@@ -7,19 +7,20 @@
 # PHPUNIT_EXCLUDE_GROUP=ParserTests mw-phpunit.sh
 # PHPUNIT_TESTSUITE=extensions mw-phpunit.sh
 
-set -e
-set -u
-
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
-PHPUNIT_DIR=${PHPUNIT_DIR:-/srv/deployment/integration/phpunit/vendor/phpunit/phpunit}
 JUNIT_DEST="$LOG_DIR/junit-mw-phpunit.xml"
 
+# The MediaWiki php wrapper defaults to using the local PHPUnit from Composer.
+PHPUNIT_DIR=${PHPUNIT_DIR:-}
 PHPUNIT_GROUP=${PHPUNIT_GROUP:-}
 PHPUNIT_EXCLUDE_GROUP=${PHPUNIT_EXCLUDE_GROUP:-}
 PHPUNIT_TESTSUITE=${PHPUNIT_TESTSUITE:-}
 
 phpunit_args=()
+
+[[ "$PHPUNIT_DIR" ]] && phpunit_args+=('--with-phpunitdir ' "${PHPUNIT_DIR}")
+
 [[ "$PHPUNIT_GROUP" ]] && phpunit_args+=('--group' "${PHPUNIT_GROUP}")
 
 # Always exclude some groups:
@@ -36,6 +37,5 @@ set -x
 cd "${MW_INSTALL_PATH}/tests/phpunit"
 
 $PHP_BIN phpunit.php \
-	--with-phpunitdir "$PHPUNIT_DIR" \
 	--log-junit $JUNIT_DEST \
 	"${phpunit_args[@]:-}"
--- bin/mw-run-phpunit-allexts.sh
+++ bin/mw-run-phpunit-allexts.sh
@@ -1,27 +1,12 @@
-#!/bin/bash -e
-
+#!/bin/bash -eu
+#
 # This script used to be a Jenkins Job Builder macro 'mw-phpunit-allexts'
 #
-# References:
-# bug: 42506
-# bug: 48147
-# Ib0fdffb97cdf237a49b43d7abaa81b81afe8c499
+# References: T44506, T50147
 
 . /srv/deployment/integration/slave-scripts/bin/mw-set-env.sh
 
 JUNIT_DEST="$LOG_DIR/junit-phpunit-allexts.xml"
-
-# Path to PHPUnit as deployed by Wikimedia deployment system.
-#
-# The path is passed to MediaWiki php wrapper using --with-phpunitdir. It
-# should match a local checkout of integration/phpunit.git repository which
-# contains PHPUnit as it is installed by Composer. The local copy should be
-# deployed on all slaves via the role::ci::slave puppet class includes:
-# deployment::target { # 'contint-production-slaves': }
-#
-#
-# WARNING: don't forget to update mw-run-phpunit-allexts.sh as well
-PHPUNIT_DIR=${PHPUNIT_DIR:-/srv/deployment/integration/phpunit/vendor/phpunit/phpunit}
 
 # We have to move to the tests/phpunit directory where suite.xml is located or
 # the relative paths referenced in that file will not get properly resolved by
@@ -29,10 +14,9 @@ PHPUNIT_DIR=${PHPUNIT_DIR:-/srv/deployment/integration/phpunit/vendor/phpunit/ph
 # The Jenkins publishers are usually expecting the .xml file to be at the root
 # of the workspace, so make sure we use an absolute path.
 
+set -x
 cd "${MW_INSTALL_PATH}/tests/phpunit"
 
-set -x
 $PHP_BIN phpunit.php \
-	--with-phpunitdir "$PHPUNIT_DIR" \
 	--log-junit "$JUNIT_DEST" \
 	--testsuite extensions
