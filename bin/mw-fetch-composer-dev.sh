44307dec06a0cd01f9227a48319a7077e5344aee
Timo Tijhof <krinklemail@gmail.com>
2016-02-13 20:33:12 +0000

    mw-fetch-composer-dev: Fixup for b077efd
    * Pass --dev flag to 'composer require'
      Shouldn't make much difference but is more semantically correct and
      makes the local modified version of vendor/composer.json more like
      the one in mediawiki-core.
    
    * Rebuild classmap after this because currently we're running an older Composer
      on integration slaves still which breaks the classmap (it unregisters all
      PSR-4 and classmap packages from the autoloader causing Psr, Monolog, CSSJanus,
      etc to be unavailable).
    
    Change-Id: I23a4691cbeb94a64425f88d54b273033fba6b9ef

--- bin/mw-fetch-composer-dev.sh
+++ bin/mw-fetch-composer-dev.sh
@@ -7,4 +7,9 @@ cd "$MW_INSTALL_PATH/vendor"
 # Load require-dev packages on top of mediawiki/vendor (T112895)
 # Once jq 1.4 is available, the following may be used instead:
 # > jq -r '.["require-dev"]|to_entries|map([.key,.value])[]|join("=")' ../composer.json
-node $(dirname $0)/../tools/composer-dev-args.js "$MW_INSTALL_PATH/composer.json" | xargs composer require --ansi --no-progress --prefer-dist -v
+node $(dirname $0)/../tools/composer-dev-args.js "$MW_INSTALL_PATH/composer.json" | xargs composer require --dev --ansi --no-progress --prefer-dist -v
+
+# FIXME: integration/composer is outdated and breaks autoloader
+# Once we're on composer 1.0.0-alpha11 or higher this might not
+# be needed anymore
+composer dump-autoload --optimize
