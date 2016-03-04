1d2c60dab7b7a2dc7f3114e2b1f48d2aa411fa05
Timo Tijhof <krinklemail@gmail.com>
2016-03-04 00:08:12 +0000

    mw-fetch-composer-dev: Add fallback for REL1_23
    Change-Id: Ie3d42d8259094f57ee94eaf23a2b56f22a5ef329

--- tools/composer-dev-args.js
+++ tools/composer-dev-args.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-var devPackages = require( process.argv[ 2 ] )[ 'require-dev' ],
-	package;
+var devPackages, package;
 
-for ( package in devPackages ) {
-	console.log( package + '=' + devPackages[ package ] );
+try {
+	devPackages = require( process.argv[ 2 ] )[ 'require-dev' ];
+	for ( package in devPackages ) {
+		console.log( package + '=' + devPackages[ package ] );
+	}
+} catch ( e ) {
+	// Back-compat for REL1_23 which doesn't have composer.json
+	console.log( 'phpunit/phpunit=3.7.37' );
 }
