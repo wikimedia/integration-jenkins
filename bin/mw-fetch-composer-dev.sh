8fdd295d37412437934b449fb1439f66d9f7b523
Antoine Musso <hashar@free.fr>
2016-05-02 21:47:33 +0200

    mw-fetch-composer-dev.sh fails when node is absent
    Bash -e ignores failures in pipe:
    
    false | true
    
    set -o pipefail to have bash abort as soon as there is an error, for
    example when 'node' is not found.
    
    Change-Id: I048d77a1510e0f615a9a9ba962a6981e012cbc15

--- bin/mw-fetch-composer-dev.sh
+++ bin/mw-fetch-composer-dev.sh
@@ -7,6 +7,7 @@ cd "$MW_INSTALL_PATH/vendor"
 # Load require-dev packages on top of mediawiki/vendor (T112895)
 # Once jq 1.4 is available, the following may be used instead:
 # > jq -r '.["require-dev"]|to_entries|map([.key,.value])[]|join("=")' ../composer.json
+set -o pipefail
 node $(dirname $0)/../tools/composer-dev-args.js "$MW_INSTALL_PATH/composer.json" | xargs composer require --dev --ansi --no-progress --prefer-dist -v
 
 # FIXME: integration/composer is outdated and breaks autoloader
