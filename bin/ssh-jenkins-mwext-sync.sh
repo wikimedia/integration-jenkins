c5942aa6e39878b1126de6a714e051a57a66af2e
Antoine Musso <hashar@free.fr>
2016-09-08 12:05:05 +0200

    Remove VE script to sync the Gerrit repo
    The issue T51846 has been fixed in Gerrit and the job removed by
    139d17c8f1c4bcf2bb761e13a6501e4d85684066.
    
    Bug: T51846
    Change-Id: I34c118fc0c2eeacce0798b7ff74ecee523262cfc

--- bin/ssh-jenkins-mwext-sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-# Fallbacks for env variables exported in gerrit-sync-ve-push.sh
-GERRIT_USER_SSH_IDENTITY=${GERRIT_USER_SSH_IDENTITY:-'/var/lib/jenkins/.ssh/jenkins-mwext-sync_id_rsa'}
-exec ssh -i "$GERRIT_USER_SSH_IDENTITY" "$@"
