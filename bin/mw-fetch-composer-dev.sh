a7728a57030bc47d1bf2df172a0a1678b278c63a
Antoine Musso <hashar@free.fr>
2017-02-24 11:55:43 +0100

    Option to merge mw composer into vendor
    PHP is apparently  not being to lookup a static method through the
    namespaces.  Eg the following does not work:
    
        use Hamcrest\Matchers;
        both( .. );
    
    For static methods, PHP requires to use at least the class, eg with the
    full canonical path it works fine:
    
        \Hamcrest\Matchers::both( ... );
    
    hamcrest/hamcrest-php and wmde/hamcrest-html-matchers have a syntastic
    sugar to expose some of their static methods as global functions. They
    can be loaded either explicitly with:
    
        Hamcrest\Util::registerGlobalFunctions()
    
    Or using a composer files autoloader which we have done in
    mediawiki/core:
    
        "autoload-dev": {
            "files": [
                "vendor/hamcrest/hamcrest-php/hamcrest/Hamcrest.php",
                "vendor/wmde/hamcrest-html-matchers/src/functions.php"
            ]
        },
    
    In the first example, both() would be resolved by PHP as the global
    function it get invoked and ends up invoking the static method. The code
    works.
    
    When using mediawiki/vendor, mw-fetch-composer-dev installs the dev
    dependencies (via composer-dev-args.js) but the autoload-dev section is
    NOT merged in. The global functions are hence not registered and the
    build fails.
    
    Add option MW_COMPOSER_MERGE_MW_IN_VENDOR which would add in the
    vendor/composer.json an extra section for the composer-merge-plugin
    instructing it to merge in mediawiki/core.  That will add the
    require-dev sections (done previously) and the autoload-dev section.
    
    That is done before 'composer dump-autoload' which would then generate
    a vendor/composer/autoload_file.php containing the files from
    mediawiki/core autoloader-dev section.
    
    Also copy (existing) resulting files to log dir so they get attached to
    the build.
    
    Bug: T158674
    Change-Id: I5fd7411560aed93a5578a8c0cf5cdcc73384d9b5

--- bin/mw-fetch-composer-dev.sh
+++ bin/mw-fetch-composer-dev.sh
@@ -10,7 +10,18 @@ cd "$MW_INSTALL_PATH/vendor"
 set -o pipefail
 node $(dirname $0)/../tools/composer-dev-args.js "$MW_INSTALL_PATH/composer.json" | xargs composer require --dev --ansi --no-progress --prefer-dist -v
 
+# Point composer-merge-plugin to mediawiki/core to have it to merge
+# autoload-dev. T158674
+if [ "${MW_COMPOSER_MERGE_MW_IN_VENDOR:-}" ] && [ -f "$MW_INSTALL_PATH/composer.json" ]; then
+	composer config extra.merge-plugin.include "$MW_INSTALL_PATH/composer.json"
+fi
+
 # FIXME: integration/composer is outdated and breaks autoloader
 # Once we're on composer 1.0.0-alpha11 or higher this might not
 # be needed anymore
 composer dump-autoload --optimize
+
+mkdir -p "$LOG_DIR"
+[ -f "$MW_INSTALL_PATH/composer.json" ] && cp -vf "$MW_INSTALL_PATH/composer.json" "$LOG_DIR/composer.core.json.txt"
+[ -f "$MW_INSTALL_PATH/vendor/composer.json" ] && cp -vf "$MW_INSTALL_PATH/vendor/composer.json" "$LOG_DIR/composer.vendor.json.txt"
+[ -f "$MW_INSTALL_PATH/vendor/composer/autoload_file.php" ] && cp -vf "$MW_INSTALL_PATH/vendor/composer/autoload_file.php" "$LOG_DIR/composer.autoload_file.php.txt"
