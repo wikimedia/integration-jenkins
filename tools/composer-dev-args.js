aa27b4ee9d992124c87eb3deca8cd9c3c48aec28
Antoine Musso <hashar@free.fr>
2017-06-01 17:12:59 +0200

    composer-dev-args: remove phpunit=3.7.37 for REL1_23
    REL1_23 is gone, hence we no more need to inject phpunit in composer dev
    requirements.  All active branches now have phpunit in their
    composer.json.
    
    Bug: T166706
    Change-Id: I0dd0d0632134466c75dadacbcb00432194ae6938

--- tools/composer-dev-args.js
+++ tools/composer-dev-args.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 var devPackages, package;
 
-try {
-	devPackages = require( process.argv[ 2 ] )[ 'require-dev' ];
-	for ( package in devPackages ) {
-		console.log( package + '=' + devPackages[ package ] );
-	}
-} catch ( e ) {
-	console.error(e.message, '=> falling back to phpunit/phpunit=3.7.37');
-	// Back-compat for REL1_23 which doesn't have composer.json
-	console.log( 'phpunit/phpunit=3.7.37' );
+devPackages = require( process.argv[ 2 ] )[ 'require-dev' ];
+for ( package in devPackages ) {
+	console.log( package + '=' + devPackages[ package ] );
 }
