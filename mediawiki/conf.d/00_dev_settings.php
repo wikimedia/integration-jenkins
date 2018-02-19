842083c2c4aa810e5f3691d58373aac51d450fe3
Krinkle <krinklemail@gmail.com>
2018-02-19 22:54:01 +0000

    mwconf: Re-enable use of built-in DevelopmentSettings.php
    This reverts commit cf84feaba2d1a7889d89e61a3ba4f5ca02ce97b7.
    
    Bug: T187581
    Bug: T177669
    Change-Id: I9f427b13e38d5132415e6e52199632a6c5170cde

--- mediawiki/conf.d/00_dev_settings.php
+++ mediawiki/conf.d/00_dev_settings.php
@@ -3,12 +3,15 @@
 # Snippet coming from integration/jenkins.git:/mediawiki/conf.d/
 #
 
+/**
+ * Determine $wmgJobWorkspace and $wmgMwLogDir
+ */
+
 // Under Apache, there is no Jenkins environement variable. We have to detect
 // the workspace using MediaWiki's include path.
 //
-// Legacy mode was to fetch MediaWiki core directly in the workspace hence
-// $IP is the workspace.
-// The new way is to fetch it under $WORKSPACE/src/mediawiki/core
+// Old Jenkins jobs fetch MediaWiki core directly in the workspace hence
+// $IP is the workspace. The new way is $WORKSPACE/src/mediawiki/core
 if ( $wgCommandLineMode ) {
 	$wmgJobWorkspace = getenv( 'WORKSPACE' );
 } elseif ( preg_match( '%(.*)/src/mediawiki/core%', $IP ) ) {
@@ -20,30 +23,45 @@ if ( $wgCommandLineMode ) {
 }
 $wmgMwLogDir = "$wmgJobWorkspace/log";
 
-// Debugging: PHP
-error_reporting( -1 );
-ini_set( 'display_errors', 1 );
-
-// Debugging: MediaWiki
-$wgDevelopmentWarnings = true;
-$wgShowDBErrorBacktrace = true;
-$wgShowExceptionDetails = true;
-$wgShowSQLErrors = true;
-$wgDebugRawPage = true; // bug 47960
-
-// Debugging: Logging
-if ( $wgCommandLineMode ) {
-	$wgDebugLogFile = "$wmgMwLogDir/mw-debug-cli.log";
+/**
+ * Development settings
+ */
+if ( is_file( "$IP/includes/DevelopmentSettings.php" ) ) {
+	putenv( "MW_LOG_DIR=$wmgMwLogDir" );
+	require_once "$IP/includes/DevelopmentSettings.php";
 } else {
-	$wgDebugLogFile = "$wmgMwLogDir/mw-debug-www.log";
+	// Compatibility with older MediaWiki branches
+
+	// Debugging: PHP
+	error_reporting( -1 );
+	ini_set( 'display_errors', 1 );
+
+	// Debugging: MediaWiki
+	$wgDevelopmentWarnings = true;
+	$wgShowDBErrorBacktrace = true;
+	$wgShowExceptionDetails = true;
+	$wgShowSQLErrors = true;
+	$wgDebugRawPage = true; // bug 47960
+
+	// Debugging: Logging
+	if ( $wgCommandLineMode ) {
+		$wgDebugLogFile = "$wmgMwLogDir/mw-debug-cli.log";
+	} else {
+		$wgDebugLogFile = "$wmgMwLogDir/mw-debug-www.log";
+	}
+	$wgDebugTimestamps = true;
+	$wgDBerrorLog = "$wmgMwLogDir/mw-dberror.log";
+	$wgDebugLogGroups['ratelimit'] = "$wmgMwLogDir/mw-ratelimit.log";
+	$wgDebugLogGroups['exception'] = "$wmgMwLogDir/mw-exception.log";
+	$wgDebugLogGroups['error'] = "$wmgMwLogDir/mw-error.log";
+	// Back-compat
+	$wgRateLimitLog = $wgDebugLogGroups['ratelimit'];
 }
-$wgDebugTimestamps = true;
-$wgDBerrorLog = "$wmgMwLogDir/mw-dberror.log";
-$wgDebugLogGroups['ratelimit'] = "$wmgMwLogDir/mw-ratelimit.log";
-$wgDebugLogGroups['exception'] = "$wmgMwLogDir/mw-exception.log";
-$wgDebugLogGroups['error'] = "$wmgMwLogDir/mw-error.log";
-// Back-compat
-$wgRateLimitLog = $wgDebugLogGroups['ratelimit'];
+
+/**
+ * Experimental settings that we're trying for Jenkins
+ * before they become defaults in MediaWiki core
+ */
 
 // Be strict about class name letter-case.
 $wgAutoloadAttemptLowercase = false;
