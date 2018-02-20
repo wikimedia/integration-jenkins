9ec66327170df552806cbec2655a3251e099f135
Reedy <reedy@wikimedia.org>
2018-02-20 16:28:32 +0000

    Remove composer-dev-args.js
    No longer used after I3ced8a41989a3e867.
    
    Depends-On: I3ced8a41989a3e867e05cf2d5b0abead52d78d56
    Change-Id: Ib91aed42f769e82f014c847e6a9bdf1d10536585

--- tools/composer-dev-args.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-var devPackages, package;
-
-devPackages = require( process.argv[ 2 ] )[ 'require-dev' ];
-for ( package in devPackages ) {
-	console.log( package + '=' + devPackages[ package ] );
-}
